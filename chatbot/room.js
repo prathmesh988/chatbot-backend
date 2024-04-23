95a7f21bfa62523f4fc3791b94a8ef077544dd39
prathmesh988 <prathmeshgawade41@gmail.com>
2024-04-23 20:39:59 +0530

    room functionality added1

--- chatbot/room.js
+++ chatbot/room.js
@@ -1,42 +1,130 @@
-const WebSocket = require("ws");
+import { init, init as initOpenAI } from "./index.js";
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import express from "express";
+import bodyParser from "body-parser";
 
-// Create a WebSocket server
-const wss = new WebSocket.Server({ port: 8080 });
+import cors from "cors";
 
-// Define a map to store connections by room
-const rooms = new Map();
+import WebSocket, { WebSocketServer } from "ws";
 
-// WebSocket server event listener
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(message) {
-    // Parse the message to JSON
-    const data = JSON.parse(message);
 
-    // Check if the message is for joining a room
-    if (data.type === "joinRoom") {
-      const roomName = data.room;
+async function engine(message) {
+  message = message.toString("utf8");
+  const res = await initOpenAI(message);
+  return res;
 
-      // Create the room if it doesn't exist
-      if (!rooms.has(roomName)) {
-        rooms.set(roomName, new Set());
+}
+
+function generateUniqueId() {
+  const uniqueID = uuidv4();
+  if (!isUniqueIdUsed(uniqueID)) {
+      storeUniqueId(uniqueID);
+      return uniqueID;
+  } else {
+      // If generated ID is not unique, recursively call the function again
+      return generateUniqueId();
+  }
+}
+
+// Check if the generated ID is already used
+function isUniqueIdUsed(id) {
+  const usedIds = getUsedIds();
+  return usedIds.includes(id);
+}
+
+// Store the generated ID
+function storeUniqueId(id) {
+  fs.appendFileSync('usedIds.txt', id + '\n');
+}
+
+// Retrieve all previously used IDs
+function getUsedIds() {
+  try {
+      const data = fs.readFileSync('usedIds.txt', 'utf8');
+      return data.split('\n').filter(id => id.trim() !== '');
+  } catch (err) {
+      // If the file doesn't exist yet, return an empty array
+      return [];
+  }
+}
+
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const httpServer = app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+const wss = new WebSocketServer({ server: httpServer });
+const rooms = {};
+
+wss.on("connection", socket => {
+  let uuid = null; 
+
+  const leave = room => {
+    if (!rooms[room] || !rooms[room][uuid]) return;
+    if (Object.keys(rooms[room]).length === 1) delete rooms[room];
+    else delete rooms[room][uuid];
+  };
+
+  socket.on("message", (data) => {
+    const receivedString = data.toString('utf-8');
+    const jsonObject = JSON.parse(receivedString);
+    console.log(jsonObject)
+    const { message, meta, room } = jsonObject;
+
+    if (meta === "join") {
+      if (!room || typeof room !== 'string') {
+        console.log("Invalid room name.");
+        return;
       }
 
-      // Add the connection to the room
-      rooms.get(roomName).add(ws);
+      uuid = generateUniqueId(); 
+      if (!rooms[room]) {
+        rooms[room] = {};
+      }
+      rooms[room][uuid] = socket;
+      console.log(`Client joined room '${room}' with UUID '${uuid}'.`);
+    } else if (meta === "leave") {
+      leave(room);
+      console.log(`Client left room '${room}' with UUID '${uuid}'.`);
+    } else if (!meta) {
+      console.log('this is calle')
+      if (!rooms[room]) {
+        console.log(`Room '${room}' does not exist.`);
+        return;
+      }
+      Object.values(rooms[room]).forEach((clientSocket) => {
+        clientSocket.send(JSON.stringify(message));
+      });
 
-      console.log(`User joined room ${roomName}`);
-    } else if (data.type === "message") {
-      // Broadcast the message to all connections in the same room
-      const roomName = data.room;
-      const message = data.message;
-
-      if (rooms.has(roomName)) {
-        rooms.get(roomName).forEach((client) => {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: "message", message }));
-          }
+      const res = engine(message);
+      res.then((dat) => {
+        const timestamp = Date.now();
+        const responseObj = {
+          dat: dat,
+          meta: "chatMessage",
+          room: room,
+          sender: uuid,
+          timestamp: timestamp
+        };
+       
+        Object.values(rooms[room]).forEach((clientSocket) => {
+          clientSocket.send(JSON.stringify(responseObj));
         });
-      }
+        console.log(`Response sent to room '${room}'.`);
+      }).catch((err) => {
+        console.error("Error processing message:", err);
+      });
     }
+  });
+
+  socket.on("close", () => {
+    Object.keys(rooms).forEach(room => leave(room));
+    console.log(`Connection closed for UUID '${uuid}'.`);
   });
 });
